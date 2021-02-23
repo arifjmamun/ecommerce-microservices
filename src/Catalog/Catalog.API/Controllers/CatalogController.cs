ed2870db3bc0ae20591ee881e4990be064c078f5
Md. Ariful Islam <arifjmamun24@gmail.com>
2021-02-23 23:01:02 +0600

    Initial commit

---
+++ src/Catalog/Catalog.API/Controllers/CatalogController.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Threading.Tasks;
+using Catalog.API.Entities;
+using Catalog.API.Repositories.Interfaces;
+using DnsClient.Internal;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+
+namespace Catalog.API.Controllers
+{
+    [Route("api/v1/[controller]")]
+    [ApiController]
+    public class CatalogController : ControllerBase
+    {
+        private readonly IProductRepository _repository;
+        private readonly ILogger<CatalogController> _logger;
+
+        public CatalogController(IProductRepository repository, ILogger<CatalogController> logger)
+        {
+            _repository = repository;
+            _logger = logger;
+        }
+
+        [HttpGet]
+        [ProducesResponseType(typeof(IEnumerable<Product>), (int)HttpStatusCode.OK)]
+        public async Task<ActionResult<IEnumerable<Product>>> GetProducts()
+        {
+            var products = await _repository.GetProducts();
+            return Ok(products);
+        }
+
+        [HttpGet("/id")]
+        [ProducesResponseType((int)HttpStatusCode.NotFound)]
+        [ProducesResponseType(typeof(Product), (int)HttpStatusCode.OK)]
+        public async Task<ActionResult<Product>> GetProduct(string id)
+        {
+            var product = await _repository.GetProduct(id);
+            if (product == null)
+            {
+                _logger.LogError($"Product with id: {id}, not found!");
+                return NotFound();
+            }
+            return Ok(product);
+        }
+    }
+}
---
+++ src/Catalog/Catalog.API/Data/CatalogContextSeed.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using Catalog.API.Entities;
+using MongoDB.Driver;
+
+namespace Catalog.API.Data
+{
+    public class CatalogContextSeed
+    {
+        public static void Seed(IMongoCollection<Product> productCollection)
+        {
+            var isProductExist = productCollection.Find(c => true).Any();
+            if (!isProductExist)
+            {
+                productCollection.InsertManyAsync(GetPreConfiguredProducts());
+            }
+        }
+
+        private static IEnumerable<Product> GetPreConfiguredProducts()
+        {
+            return new List<Product>
+            {
+                new Product
+                {
+                    Name = "Xiaomi Mi 9",
+                    Summary = "This phone is the company's biggest change to its flagship smartphone in years.",
+                    Description = "Lorem ipsum",
+                    ImageFile = "product-5.png",
+                    Price = 380.00M,
+                    Category = "Smartphone"
+                },
+                new Product
+                {
+                    Name = "iPhone X2",
+                    Summary = "This phone is the company's biggest change to its flagship smartphone in years.",
+                    Description = "Lorem ipsum",
+                    ImageFile = "product-5.png",
+                    Price = 680.00M,
+                    Category = "Smartphone"
+                },
+                new Product
+                {
+                    Name = "iPhone X2 Pro",
+                    Summary = "This phone is the company's biggest change to its flagship smartphone in years.",
+                    Description = "Lorem ipsum",
+                    ImageFile = "product-5.png",
+                    Price = 880.00M,
+                    Category = "Smartphone"
+                }
+            };
+        }
+    }
+}
