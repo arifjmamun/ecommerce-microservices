4b88dea8986529c22e53f348841a6c3cbf11fe6e
Md. Ariful Islam <arifjmamun24@gmail.com>
2021-02-27 00:11:41 +0600

    Added references to Order.API project and configured order seed

---
+++ src/Ordering/Ordering.Infrastructure/Data/OrderContextSeed.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Logging;
+using Ordering.Core.Entities;
+
+namespace Ordering.Infrastructure.Data
+{
+    public class OrderContextSeed
+    {
+        private static IEnumerable<Order> GetPreConfiguredOrders()
+        {
+            return new List<Order>
+            {
+                new Order
+                {
+                    UserName = "swn",
+                    FirstName = "swn",
+                    LastName = "swn",
+                    EmailAddress = "arif.dev@outlook.com",
+                    AddressLine = "Dhaka, Mirpur 12",
+                    Country = "Bangladesh"
+                }
+            };
+        }
+        public static async Task SeedAsync(OrderContext orderContext, ILoggerFactory loggerFactory, int? retry = 0)
+        {
+            int retryForAvailibility = retry.Value;
+            try
+            {
+                orderContext.Database.Migrate();
+                if (!orderContext.Orders.Any())
+                {
+                    orderContext.Orders.AddRange(GetPreConfiguredOrders());
+                    await orderContext.SaveChangesAsync();
+                }
+            }
+            catch (Exception ex)
+            {
+                if (retryForAvailibility < 1)
+                {
+                    retryForAvailibility++;
+                    var log = loggerFactory.CreateLogger<OrderContextSeed>();
+                    log.LogError(ex.Message);
+                    await SeedAsync(orderContext, loggerFactory, retryForAvailibility);
+                }
+            }
+        }
+    }
+}
