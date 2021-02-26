8bce7077b1b971a445091ed7b008f6ee398762bc
Md. Ariful Islam <arifjmamun24@gmail.com>
2021-02-26 23:50:58 +0600

    Added entity framework with its related dependencies

---
+++ src/Ordering/Ordering.Infrastructure/Data/OrderContext.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore;
+
+namespace Ordering.Infrastructure.Data
+{
+    public class OrderContext : DbContext
+    {
+        public OrderContext(DbContextOptions<OrderContext> options) : base(options)
+        {
+        }
+    }
+}
