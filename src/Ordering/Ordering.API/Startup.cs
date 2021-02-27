f9ec773f138bba0b2f70d2d15b06d8ae4042e587
Md. Ariful Islam <arifjmamun24@gmail.com>
2021-02-27 23:16:03 +0600

    Added missing OrderRepository implementation

--- src/Ordering/Ordering.API/Startup.cs
+++ src/Ordering/Ordering.API/Startup.cs
@@ -1,3 +1,5 @@
+using System.Reflection;
+using MediatR;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.EntityFrameworkCore;
@@ -5,7 +7,12 @@ using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
 using Microsoft.OpenApi.Models;
+using Ordering.Application.Handlers;
+using Ordering.Core.Repositories;
+using Ordering.Core.Repositories.Base;
 using Ordering.Infrastructure.Data;
+using Ordering.Infrastructure.Repositories;
+using Ordering.Infrastructure.Repositories.Base;
 
 namespace Ordering.API
 {
@@ -21,10 +28,17 @@ namespace Ordering.API
         // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
         {
-            services.AddDbContext<OrderContext>(
-                c => c.UseSqlServer(Configuration.GetConnectionString("OrderConnection")),
+            services.AddDbContext<OrderContext>(c =>
+                c.UseSqlServer(Configuration.GetConnectionString("OrderConnection")),
                 ServiceLifetime.Singleton
             );
+            services.AddAutoMapper(typeof(Startup));
+            services.AddMediatR(typeof(CheckoutOrderHandler).GetTypeInfo().Assembly);
+
+            services.AddTransient<IOrderRepository, OrderRepository>();
+            services.AddScoped(typeof(IRepository<>), typeof(Repository<>));
+            services.AddScoped(typeof(IOrderRepository), typeof(OrderRepository));
+
             services.AddControllers();
 
             services.AddSwaggerGen(c =>
